83b804a8611510a434a3f25749a802559bae8f91
zaraamerr <zaraa.amerr@gmail.com>
2023-02-02 21:28:25 -0500

    removed namespace and explicitly referred to std for best practice

--- leap.cpp
+++ leap.cpp
@@ -7,18 +7,17 @@
 //should print Leap year, otherwise, print Common year.
 
 #include <iostream>
-using namespace std;
 
 int main(){
     int year; //create a var that will store user's input for year number
-    cout << "Input a year number: " << endl; //ask user for year number
-    cin >> year; //store user's input into var
+    std::cout << "Input a year number: " << std::endl; //ask user for year number
+    std::cin >> year; //store user's input into var
     //leap year if perfectly divisible by 4 and not divisible by 100, or just divisible by 400
     if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
-        cout << year << " is a leap year" << endl;
+        std::cout << year << " is a leap year" << std::endl;
     }
     else{
-        cout << year << " is a common year" << endl;  
+        std::cout << year << " is a common year" << std::endl;  
     }
     
     return 0;
--- month.cpp
+++ month.cpp
@@ -6,28 +6,27 @@
 //(taking into account leap years). 
 
 #include <iostream>
-using namespace std;
 
 int main(){
     int year; //create var that stores user's year input
     int month; //create a var that stores user's month input
-    cout << "Enter a year: "; //ask user to enter a year
-    cin >> year; //store user input into year
-    cout << "Enter a month (1-12): "; //ask user to enter a month
-    cin >> month; //store user input into month
+    std::cout << "Enter a year: "; //ask user to enter a year
+    std::cin >> year; //store user input into year
+    std::cout << "Enter a month (1-12): "; //ask user to enter a month
+    std::cin >> month; //store user input into month
     if (month == 2){ //if the month is february
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){ //if its a leap year
-            cout << "29 days" << endl; //output that february has 29 days
+            std::cout << "29 days" << std::endl; //output that february has 29 days
         }
         else{ //if not a leap year
-            cout << "28 days" << endl; //output that february has 28 days
+            std::cout << "28 days" << std::endl; //output that february has 28 days
         }
     }
     else if (month == 4 || month == 6 || month == 9 || month == 11){ //if month is april, june, september, or november
-        cout << "30 days" << endl; //output 30 days since those months have 30
+        std::cout << "30 days" << std::endl; //output 30 days since those months have 30
     }
     else{ //if any other month in the calendar
-        cout << "31 days" << endl; //output 31 days
+        std::cout << "31 days" << std::endl; //output 31 days
     }
     return 0;
 }
